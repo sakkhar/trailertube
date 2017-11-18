0f75a3bc0de7a670a89f73a810fbc79ff8baca9f
sahasakkhar <sakkhar@mail.com>
2017-11-18 13:51:51 +0600

    TrailerTube is a trailer-sharing website.Most of the content on TrailerTube has been linked from YouTube.Unregistered users can watch videos, and registered users are permitted to upload an unlimited number of videos.

--- trailertube/urls.py
+++ trailertube/urls.py
@@ -15,7 +15,20 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+from tube.views import Video, add_video, message
+from accounts.views import login_view, register_view, logout_view
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', Video, name='video'),
+    url(r'^upload/', add_video, name='upload'),
+    url(r'^message/', message, name='message'),
+    url(r'^login/', login_view, name='login'),
+    url(r'^logout/', logout_view, name='logout'),
+    url(r'^join/', register_view, name='join'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
